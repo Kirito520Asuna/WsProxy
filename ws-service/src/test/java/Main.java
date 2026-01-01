ad900f939cedef213ced27d65c560f1a49c7b49f
yan <doutianmianxia@qq.com>
2026-01-01 13:04:45 +0800

    fix(api): 修复API端点路径错误
    - 修正了消息发送API端点路径从/ws-proxy/message/api/send到/ws-proxy/api/message/send
    - 修正了JWT消息发送API端点路径从/ws-proxy/message/jwt/send到/ws-proxy/jwt/message/send

--- ws-service/src/test/java/Main.java
+++ ws-service/src/test/java/Main.java
@@ -11,11 +11,11 @@ public class Main {
     public static void main(String[] args) {
         WsProxy wsProxy = new WsProxy();
         String action = "send_private_msg";//私发 //send_group_msg //群发
-        String userId = "1608362555";
+        String userId = "qq";
         String test = "xxxxxx";
         String type = "text";//
         String urlWsProxy = "http://127.0.0.1:8081/ws-proxy/message/send/";//
-        String url = "ws://39.108.183.241:3001/";//
+        String url = "ws://xxxxx:3001/";//
         String token = "ws-token";//
         String body = "{\n" +
                 "        \"action\": \"" + action + "\",\n" +
